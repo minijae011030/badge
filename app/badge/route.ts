b0f8bb3cc5a939f7084a0b41cf909884b3c4cc78
Ian <105128049+minijae011030@users.noreply.github.com>
2025-05-08 14:13:59 +0900

    Feat: html -> svg

--- app/badge/route.ts
+++ app/badge/route.ts
@@ -4,20 +4,24 @@ export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const text = searchParams.get("text") ?? "Default";
 
-  const html = `
-    <div style="
-      display: inline-block;
-      padding: 3px 8px;
-      border-radius: 5px;
-      background-color: gray;
-      color: white;
-      font-family: sans-serif;
-      font-size: 12px;
-    ">${text}</div>`;
+  const svg = `
+  <svg xmlns="http://www.w3.org/2000/svg" height="24" role="img">
+    <rect rx="5" height="24" width="${text.length * 8 + 16}" fill="gray"/>
+    <text
+      x="50%"
+      y="50%"
+      dominant-baseline="middle"
+      text-anchor="middle"
+      font-family="sans-serif"
+      font-size="12"
+      fill="white"
+    >${text}</text>
+  </svg>`;
 
-  return new NextResponse(html, {
+  return new NextResponse(svg, {
     headers: {
-      "Content-Type": "text/html",
+      "Content-Type": "image/svg+xml",
+      "Cache-Control": "no-cache",
     },
   });
 }
